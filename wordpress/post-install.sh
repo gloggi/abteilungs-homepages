be392cce9fc695c929d642be87a05929e7370736
Carlo Beltrame <beltrame@puzzle.ch>
2017-09-30 13:27:03 +0200

    Set some options by standard

--- wordpress/post-install.sh
+++ wordpress/post-install.sh
@@ -18,6 +18,29 @@ then
 	runuser www-data -s /bin/sh -c 'wp option update show_on_front page'
 	runuser www-data -s /bin/sh -c 'wp option update page_on_front $(wp post list --post_type=page --field=ID | head -n 1)'
 
+	# Set some options in options-general.php
+	runuser www-data -s /bin/sh -c 'wp option update users_can_register 0'
+	runuser www-data -s /bin/sh -c 'wp option update WPLANG de_CH_informal'
+	runuser www-data -s /bin/sh -c 'wp option update timezone_string Europe/Zurich'
+	runuser www-data -s /bin/sh -c 'wp option update date_format d.m.Y'
+	runuser www-data -s /bin/sh -c 'wp option update time_format H:i'
+
+	# Set some options in options-discussion.php
+	runuser www-data -s /bin/sh -c 'wp option update default_pingback_flag 0'
+	runuser www-data -s /bin/sh -c 'wp option update default_ping_status 0'
+	runuser www-data -s /bin/sh -c 'wp option update default_comment_status 0'
+	runuser www-data -s /bin/sh -c 'wp option update comment_registration 1'
+	runuser www-data -s /bin/sh -c 'wp option update close_comments_for_old_posts 1'
+	runuser www-data -s /bin/sh -c 'wp option update close_comments_days_old 0'
+	runuser www-data -s /bin/sh -c 'wp option update thread_comments 0'
+	runuser www-data -s /bin/sh -c 'wp option update comments_notify 0'
+	runuser www-data -s /bin/sh -c 'wp option update moderation_notify 0'
+	runuser www-data -s /bin/sh -c 'wp option update comment_moderation 1'
+	runuser www-data -s /bin/sh -c 'wp option update comment_whitelist 0'
+
+	# Set some options in options-media.php
+	runuser www-data -s /bin/sh -c 'wp option update uploads_use_yearmonth_folders 0'
+
 else
 
 	echo 'This container has been started before'
