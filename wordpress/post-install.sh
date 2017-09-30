e30b9aec0368f650501aa6d7e114e6afd8482264
Carlo Beltrame <beltrame@puzzle.ch>
2017-09-30 12:01:06 +0200

    Automatically set a page as front page

--- wordpress/post-install.sh
+++ wordpress/post-install.sh
@@ -7,5 +7,10 @@
 runuser www-data -s /bin/sh -c 'wp plugin list --status=inactive --field=name | egrep -v "gloggi" | xargs -0 -d"\n" wp plugin uninstall'
 runuser www-data -s /bin/sh -c 'wp theme list --status=inactive --enabled=no --field=name | egrep -v "gloggi" | xargs -0 -d"\n" wp theme delete'
 
+# Set a static front page - any page we can find
+runuser www-data -s /bin/sh -c 'wp option update show_on_front page'
+runuser www-data -s /bin/sh -c 'wp option update page_on_front $(wp post list --post_type=page --field=ID | head -n 1)'
+
+
 # This command must be last in this file, it runs the Wordpress server
 apache2-foreground
