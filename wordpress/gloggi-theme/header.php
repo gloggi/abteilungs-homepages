02980a1b476ab00a372fdec1f573bb03477472ad
Carlo Beltrame <beltrame@puzzle.ch>
2017-10-11 14:01:59 +0200

    Add dynamic drop shadow to banner logo

--- wordpress/gloggi-theme/header.php
+++ wordpress/gloggi-theme/header.php
@@ -4,7 +4,8 @@
 <div class="header">
     <div class="header__image"></div>
     <div class="header__banner">
-        <img class="header__logo-large" src="<?php echo wpod_get_option( 'gloggi_einstellungen', 'abteilungslogo' ); ?>" alt="">
+        <svg xmlns="http://www.w3.org/2000/svg"><defs><filter id="shadow"><feDropShadow stdDeviation="4"/></filter></defs></svg>
+        <img class="header__logo-large" src="<?php echo wpod_get_option( 'gloggi_einstellungen', 'abteilungslogo' ); ?>" alt="" style="filter:url(#shadow)"/>
         <h1 class="header__title"><?php echo wpod_get_option( 'gloggi_einstellungen', 'abteilung'); ?></h1>
     </div>
     <h1 class="header__heading"><?php echo get_the_title( $post ); ?></h1>
