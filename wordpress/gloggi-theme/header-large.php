5bc2647c0d8cbeffd1c2004b13ab72190853c609
Carlo Beltrame <beltrame@puzzle.ch>
2017-10-30 15:20:01 +0100

    Fix vertical centering of home page banner logo

--- wordpress/gloggi-theme/header-large.php
+++ wordpress/gloggi-theme/header-large.php
@@ -4,7 +4,7 @@
 <div class="header-large">
     <div class="header__image-large"></div>
     <div class="header__banner-large">
-        <svg xmlns="http://www.w3.org/2000/svg"><defs><filter id="shadow"><feDropShadow stdDeviation="4"/></filter></defs></svg>
+        <svg xmlns="http://www.w3.org/2000/svg" style="height:0"><defs><filter id="shadow"><feDropShadow stdDeviation="4"/></filter></defs></svg>
         <img class="header__logo-large" src="<?php echo wpod_get_option( 'gloggi_einstellungen', 'abteilungslogo' ); ?>" alt="" style="filter:url(#shadow)"/>
         <h1 class="header__title"><?php echo wpod_get_option( 'gloggi_einstellungen', 'abteilung'); ?></h1>
     </div>
