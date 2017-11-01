86e52edb106a5f1ff331264f9c360fb20609baa9
Carlo Beltrame <beltrame@puzzle.ch>
2017-11-01 10:07:50 +0100

    Fix logo in navigation and page templates

--- wordpress/gloggi-theme/header.php
+++ wordpress/gloggi-theme/header.php
@@ -5,7 +5,7 @@
     <?php echo get_the_post_thumbnail( $post->ID, array(), array('class' => 'header__image-large' ) ); ?>
     <div class="header__banner">
         <svg xmlns="http://www.w3.org/2000/svg" style="height:0"><defs><filter id="shadow"><feDropShadow stdDeviation="4"/></filter></defs></svg>
-        <img class="header__logo" src="<?php echo wpod_get_option( 'gloggi_einstellungen', 'abteilungslogo' ); ?>" alt="" style="filter:url(#shadow)"/>
+        <img class="header__logo" src="<?php echo wp_get_attachment_url(wpod_get_option( 'gloggi_einstellungen', 'abteilungslogo' )); ?>" alt="" style="filter:url(#shadow)"/>
         <h1 class="header__title"><?php echo wpod_get_option( 'gloggi_einstellungen', 'abteilung'); ?></h1>
     </div>
     <h1 class="header__heading"><?php echo get_the_title( $post ); ?></h1>
