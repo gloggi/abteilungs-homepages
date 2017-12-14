3f9fbccaa21fd388e7f049365fc1909693c41abe
Carlo Beltrame <beltrame@puzzle.ch>
2017-12-14 14:33:28 +0100

    Fix a bug in the header

--- wordpress/gloggi-theme/header.php
+++ wordpress/gloggi-theme/header.php
@@ -2,7 +2,7 @@
 <?php get_template_part('html-head'); ?>
 <div class="header">
     <svg xmlns="http://www.w3.org/2000/svg" style="height:0; width:0; position: fixed;"><defs><filter id="shadow"><feDropShadow stdDeviation="4"/></filter></defs></svg>
-    <?php echo get_the_post_thumbnail( $post->ID, array(), array('class' => 'header__image-large' ) ); ?>
+    <?php echo get_the_post_thumbnail( $post->ID, array(), array('class' => 'header__image' ) ); ?>
     <div class="header__banner">
         <img class="header__logo" src="<?php echo wp_get_attachment_url(wpod_get_option( 'gloggi_einstellungen', 'abteilungslogo' )); ?>" alt="" style="filter:url(#shadow)"/>
         <h1 class="header__title"><?php echo wpod_get_option( 'gloggi_einstellungen', 'abteilung'); ?></h1>
