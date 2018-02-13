gloggi/abteilungs-homepages

commit 37b2a71e7453356f22dca8e3afe7ce33f4cc390f

Use full width of lightbox for upcoming special event list

frontend/src/css/_agenda.scss
@@ -72,7 +72,6 @@
 
 
 .agenda__entry-content{
-    width: 70%;
     margin-left: 20px;
     color: $font-color;
     margin-bottom: 20px;
@@ -284,4 +283,9 @@
 
 .agenda__anualplan{
     max-width: 100px;
+}
+
+.agenda__specialevent-list-entry {
+    width: 100%;
+    padding-right: 0;
 }

frontend/src/css/_lightbox.scss
@@ -27,8 +27,6 @@
     display: block;
 }
 
-
-
 .lightbox__content-wrapper{
     width: 100vw;
     height: 100vh;
@@ -54,7 +52,6 @@
     p, a, li, b {
         font-size: 18px;
     }
-    pading: 10px;
 }
 
 .lightbox__banner{
@@ -66,10 +63,7 @@
 .lightbox__body{
     height: 90%;
     overflow-y: scroll;
-}
-
-.lightbox__section{
-    margin: 30px;
+    padding: 30px;
 }
 
 .lightbox__footer{

wordpress/gloggi-theme/agenda.php
@@ -317,7 +317,7 @@ $specialevent_titel = get_the_title(); ?>
             <?php if( $specialevent_anlaesse[$specialevent_titel] && count($specialevent_anlaesse[$specialevent_titel] > 0) ) : ?>
             <div class="lightbox__section"><h3>N&auml;chste <?php echo wpptd_get_post_meta_value( $post->ID, 'pluralname' ); ?></h3></div>
             <?php foreach( $specialevent_anlaesse[$specialevent_titel] as $specialevent_anlass ) : ?>
-            <div class="lightbox__section agenda__entry <?php echo $specialevent_anlass['anlassgruppen_classes']; ?>" data-starttime="<?php echo $specialevent_anlass['startzeit']; ?>">
+            <div class="agenda__specialevent-list-entry lightbox__section agenda__entry <?php echo $specialevent_anlass['anlassgruppen_classes']; ?>" data-starttime="<?php echo $specialevent_anlass['startzeit']; ?>">
               <a href="#agenda-entry-<?php echo $specialevent_anlass['ID']; ?>">
                 <div class="circle-small color-primary" style="<?php if( $specialevent_anlass['anlassfarbe'] ) : echo 'background-color: ' . $specialevent_anlass['anlassfarbe'] . ' !important;'; endif; ?>">
                   <?php if( $specialevent_anlass['anlasslogo'] ) : ?><img src="<?php echo $specialevent_anlass['anlasslogo']; ?>" alt=""><?php else: ?><p><?php echo date_format( $specialevent_anlass['startzeitpunkt'], 'j.n.y' ); ?></p><?php endif; ?>

wordpress/gloggi-theme/files/css/main.css
@@ -201,7 +201,6 @@ ul {
       flex-direction: column; } }
 
 .agenda__entry-content {
-  width: 70%;
   margin-left: 20px;
   color: #4A4A4A;
   margin-bottom: 20px; }
@@ -364,6 +363,10 @@ ul {
 
 .agenda__anualplan {
   max-width: 100px; }
+
+.agenda__specialevent-list-entry {
+  width: 100%;
+  padding-right: 0; }
 
 .button, .button--inactive {
   font-size: 24px;
@@ -761,8 +764,7 @@ label {
   height: 90%;
   display: flex;
   flex-flow: column;
-  justify-content: flex-start;
-  pading: 10px; }
+  justify-content: flex-start; }
   @media screen and (max-width: 700px) {
     .lightbox__content {
       width: 100%;
@@ -775,10 +777,8 @@ label {
 
 .lightbox__body {
   height: 90%;
-  overflow-y: scroll; }
-
-.lightbox__section {
-  margin: 30px; }
+  overflow-y: scroll;
+  padding: 30px; }
 
 .lightbox__footer {
   display: flex;

commit b6ec64fe336cea6a4edfc0b3f35d2f18012361ed

Restore full width of agenda entry maps. Fixes a regression from 37b2a71e7453356f22dca8e3afe7ce33f4cc390f

frontend/src/css/_agenda.scss
@@ -168,6 +168,7 @@
 
 .agenda__body {
     justify-content: space-between;
+    padding: 30px;
 
     p{
         text-align: justify;

frontend/src/css/_lightbox.scss
@@ -63,7 +63,6 @@
 .lightbox__body{
     height: 90%;
     overflow-y: scroll;
-    padding: 30px;
 }
 
 .lightbox__footer{

frontend/src/css/_wwa.scss
@@ -85,6 +85,10 @@
     flex-wrap: wrap;
 }
 
+.groupdetail__body {
+    padding: 30px;
+}
+
 .contact{
     width: auto;
     display: flex;

wordpress/gloggi-theme/agenda.php
@@ -213,26 +213,28 @@ if( $anlaesse->have_posts() ) : ?>
             </div>
             <img src="<?php echo wp_get_attachment_url(wpod_get_option( 'gloggi_einstellungen', 'abteilungslogo' )); ?>" height="50" alt="">
           </div>
-          <div class="lightbox__body agenda__body">
+          <div class="lightbox__body">
             <div class="agenda__map" data-address1="<?php echo $startort['coords']; ?>" data-address2="<?php echo $endort['coords']; ?>">
             </div>
-            <div class="lightbox__section"><p class="wysiwyg"><?php echo $anlassinfos['beschreibung']; ?></p></div>
-            <div class="lightbox__section"><p class="wysiwyg">Hast du noch Fragen? Dann melde dich bei <a href="<?php echo encode_all_to_htmlentities( 'mailto:' . $anlassverantwortlicher );?>"><?php echo encode_all_to_htmlentities( $anlassverantwortlicher_name );?></a><?php if( $anlassinfos['is-specialevent'] ) : ?> oder lies allgemeine Informationen über <a href="#special-event-<?php echo sanitize_title( $specialevent_titel ); ?>"><?php echo $specialevent_pluralname; ?></a><?php endif; ?>.</p></div>
-            <div class="lightbox__section">
-              <div class="content__two-columns content__columns--1-1">
+            <div class="agenda__body">
+              <div class="lightbox__section"><p class="wysiwyg"><?php echo $anlassinfos['beschreibung']; ?></p></div>
+              <div class="lightbox__section"><p class="wysiwyg">Hast du noch Fragen? Dann melde dich bei <a href="<?php echo encode_all_to_htmlentities( 'mailto:' . $anlassverantwortlicher );?>"><?php echo encode_all_to_htmlentities( $anlassverantwortlicher_name );?></a><?php if( $anlassinfos['is-specialevent'] ) : ?> oder lies allgemeine Informationen über <a href="#special-event-<?php echo sanitize_title( $specialevent_titel ); ?>"><?php echo $specialevent_pluralname; ?></a><?php endif; ?>.</p></div>
+              <div class="lightbox__section">
+                <div class="content__two-columns content__columns--1-1">
 <?php if( $anlassinfos['mitnehmen'] ) : ?>
-                <div><h4>Mitnehmen</h4><p class="wysiwyg"><?php echo $anlassinfos['mitnehmen']; ?></p></div>
+                  <div><h4>Mitnehmen</h4><p class="wysiwyg"><?php echo $anlassinfos['mitnehmen']; ?></p></div>
 <?php endif; ?>
 <?php if( $downloads && count( $downloads ) ) : ?>
-                <div>
-                  <h4>Downloads</h4>
-                  <ul>
+                  <div>
+                    <h4>Downloads</h4>
+                    <ul>
 <?php foreach( $downloads as $download ) : ?>
-                    <li><a href="<?php echo wp_get_attachment_url( $download['download'] ); ?>"><?php echo $download['name']; ?></a></li>
+                      <li><a href="<?php echo wp_get_attachment_url( $download['download'] ); ?>"><?php echo $download['name']; ?></a></li>
 <?php endforeach; ?>
-                  </ul>
-                </div>
+                    </ul>
+                  </div>
 <?php endif; ?>
+                </div>
               </div>
             </div>
           </div>

wordpress/gloggi-theme/files/css/main.css
@@ -274,7 +274,8 @@ ul {
         font-size: 16px; } }
 
 .agenda__body {
-  justify-content: space-between; }
+  justify-content: space-between;
+  padding: 30px; }
   .agenda__body p {
     text-align: justify;
     font-size: 18px;
@@ -777,8 +778,7 @@ label {
 
 .lightbox__body {
   height: 90%;
-  overflow-y: scroll;
-  padding: 30px; }
+  overflow-y: scroll; }
 
 .lightbox__footer {
   display: flex;
@@ -1007,6 +1007,9 @@ label {
   justify-content: space-around;
   align-items: center;
   flex-wrap: wrap; }
+
+.groupdetail__body {
+  padding: 30px; }
 
 .contact {
   width: auto;

wordpress/gloggi-theme/werwirsind.php
@@ -159,7 +159,7 @@ foreach( $stufen as $stufe ) : ?>
               <img src="<?php echo wp_get_attachment_url( $gruppe['logo'] ); ?>"></img>
             </div>
           </div>
-          <div class="lightbox__body">
+          <div class="lightbox__body groupdetail__body">
             <div class="lightbox__section">
               <?php if( $gruppe['beschreibung'] ) : ?><div class="content__columns content__columns--1-1">
                 <div class="content__column">