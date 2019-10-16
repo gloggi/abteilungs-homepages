fb1337d5c91ed069d11a8bc33dea1c2a8ef713a2
Carlo Beltrame <carlo@beltra.me>
2019-10-16 19:23:03 +0200

    Switch UI language to german

---
+++ laravel/resources/lang/de/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'password' => 'Passwort muss mindestens 8 Zeichen lang und in beiden Feldern gleich sein.',
+    'reset' => 'Dein Passwort wurde zurückgesetzt!',
+    'sent' => 'Wir haben dir einen Link zum zurücksetzen des Passworts gesendet.',
+    'token' => 'Dieses Token ist zum Zurücksetzen des Passworts ungültig.',
+    'user' => 'Wir können keinen Benutzer mit dieser E-Mail-Adresse finden.',
+
+];
